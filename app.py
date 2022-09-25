cc4c80dec248dd459b22bc0ed6996d1d141b6504
Luke-DT11 <luke.ar.powell@btinternet.com>
2022-09-25 15:01:25 +0100

    improved formatting and complete functionality

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ class Todo(db.Model):
     content = db.Column(db.String(200),nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
     date_updated = db.Column(db.DateTime, default=datetime.utcnow)
-    status = db.Column(db.Integer, default=0)
+    status = db.Column(db.String(20), default="To do")
     def __repr__(self):
         return '<Task %r>' % self.id
 
@@ -62,9 +62,9 @@ def update(id):
 @app.route("/complete/<int:id>", methods=["GET","POST"])
 def complete(id):
     task = Todo.query.get_or_404(id)
-    if task.status == 0:
+    if task.status == "To do":
         try:
-            task.status = 1
+            task.status = "Complete"
             task.date_updated = datetime.utcnow()
             db.session.commit()
             return redirect("/")
@@ -72,7 +72,7 @@ def complete(id):
             return "There was an issue updating the status your task"
     else:
         try:
-            task.status = 0
+            task.status = "To do"
             task.date_updated = datetime.utcnow()
             db.session.commit()
             return redirect("/")
